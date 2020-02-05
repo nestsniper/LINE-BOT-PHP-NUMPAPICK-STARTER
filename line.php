a5ffc1ef1ceeae3ef9d70fd5f3cfe46864dbf941
nestsniper <59833308+nestsniper@users.noreply.github.com>
2020-02-05 14:49:04 +0700

    Update line.php

--- line.php
+++ line.php
@@ -3,7 +3,7 @@
 
 function send_LINE($msg){
 //  $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
-  $access_token = 'R812ciyvgxcZ33biWF1VkLkP8IsM1MDj+cf8SPSZF6P1cbSsJe+HgEMVhrmC4KE1gp4uqClaobcH4HCcCC561VgK5ygI0WW5hfNc4ZuZqPcuU9xmumCbdL8GIiOOv4i3t6MNE4VX3Brli7VOYbrd/gdB04t89/1O/w1cDnyilFU=';
+  $access_token = 'Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJXT8ZyX33/PRM0bSUwxXhcQ5FBDXjR3QdB04t89/1O/w1cDnyilFU=';
   $messages = [
         'type' => 'text',
         'text' => $msg
