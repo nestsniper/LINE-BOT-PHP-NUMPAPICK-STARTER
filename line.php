181e8c1bf423df8072172b278c7e969deb175b72
nestsniper <59833308+nestsniper@users.noreply.github.com>
2020-02-11 13:51:20 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'Uf00004aab9254c39ade77e1b2a0772d0',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
