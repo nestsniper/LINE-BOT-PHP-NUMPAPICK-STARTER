8ec91eb9ab7dd83fcf3a867a189bb7c615f72ad8
nestsniper <59833308+nestsniper@users.noreply.github.com>
2020-02-18 13:09:16 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,37 +1,38 @@
  <?php
   
+send_LINE("Test from suchart pc...");
 
-function send_LINE($msg){
-//  $access_token = 'Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJX';
-  $access_token = 'Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJX';
-  $messages = [
-        'type' => 'text',
-        'text' => $msg
-        //'text' => $text
-      ];
+ function send_LINE($msg){
+ //  $access_token = 'Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJX';
+   $access_token = 'Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJX';
+   $messages = [
+         'type' => 'text',
+         'text' => $msg
+         //'text' => $text
+       ];
 
-      // Make a POST Request to Messaging API to reply to sender
- 
-      //$url = 'https://suchartk2.herokuapp.com/bot.php';
-      $url = 'https://api.line.me/v2/bot/message/push';
-      $data = [
+       // Make a POST Request to Messaging API to reply to sender
 
-        'to' => 'Uf00004aab9254c39ade77e1b2a0772d0',
-        'messages' => [$messages],
-      ];
-      $post = json_encode($data);
-      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+       //$url = 'https://suchartk2.herokuapp.com/bot.php';
+       $url = 'https://api.line.me/v2/bot/message/push';
+       $data = [
 
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      $result = curl_exec($ch);
-      curl_close($ch);
+         'to' => 'Uf00004aab9254c39ade77e1b2a0772d0',
+         'messages' => [$messages],
+       ];
+       $post = json_encode($data);
+       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-      echo $result . "\r\n"; 
-}
+       $ch = curl_init($url);
+       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+       $result = curl_exec($ch);
+       curl_close($ch);
+
+       echo $result . "\r\n"; 
+ }
 
 ?>
