76b7e72716c6c9b7a518f0f69f812d8b58d35009
nestsniper <59833308+nestsniper@users.noreply.github.com>
2020-02-19 17:33:54 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token ='Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJXT8ZyX33/PRM0bSUwxXhcQ5FBDXjR3QdB04t89/1O/w1cDnyilFU=';
+$access_token ='Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiloDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdluCJlybNJXT8ZyX33/PRM0bSUwxXhcQ5FBDXjR3QdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://suchartk2.herokuapp.com/bot.php';
 
