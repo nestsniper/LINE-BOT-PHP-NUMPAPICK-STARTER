7e6a7ee871d741a868ca3c1cf85bc041832cf2bd
nestsniper <59833308+nestsniper@users.noreply.github.com>
2020-02-19 20:04:15 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,9 +2,9 @@
 
  function send_LINE($msg){
   
-  //$access_token = 'Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJX';
-  //$access_token = 'Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJX';
-  $access_token = 'Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiloDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdluCJlybNJXT8ZyX33/PRM0bSUwxXhcQ5FBDXjR3QdB04t89/1O/w1cDnyilFU='; 
+  
+  //$access_token = 'Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJXT8ZyX33/PRM0bSUwxXhcQ5FBDXjR3QdB04t89/1O/w1cDnyilFU=';
+  $access_token = 'Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJXT8ZyX33/PRM0bSUwxXhcQ5FBDXjR3QdB04t89/1O/w1cDnyilFU='; 
   $messages = [
          'type' => 'text',
          'text' => $msg
