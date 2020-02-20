322f143f96985fe1a0a94ccfcf4f13fcefba01de
nestsniper <59833308+nestsniper@users.noreply.github.com>
2020-02-20 16:37:29 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,7 +1,7 @@
 <?php
 $access_token ='Ehdbh7r5LudCZCM0lytY+4hW9qe6ewf388+DGDiIoDHEpsH2jO8UH+0N+DqOrQdj+CAKx2c6lWScJ4MWfHwSJSfgoFLzzwKFn3jp4Nqdpb2gBdIuCJlybNJXT8ZyX33/PRM0bSUwxXhcQ5FBDXjR3QdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://suchartk2.herokuapp.com/bot.php';
+$url = 'https://api.line.me/v2/bot/message/push';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
